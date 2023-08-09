ebb9c96bc58ff208be675a1909ac0213f768f5c5
tgoky <tgokyicloud6@gmail.com>
2023-08-09 09:05:50 +0100

    chore:dev tool contractAddr detected

--- src/Staking.jsx
+++ src/Staking.jsx
@@ -1,40 +1,103 @@
 import React, { useState, useEffect } from "react";
-import "./Staking.css"; // Import your CSS file
+import { ethers } from "ethers";
+import "./Staking.css";
+import stakingABI from "./ABI/staking.json"; // Replace with the correct path to your staking ABI
+import tokenABI from "./abi.json"; // Replace with the correct path to your token ABI
+import { useWeb3React } from "@web3-react/core";
 
-const Staking = ({ contract, account }) => {
+const tokenAddress = '0xddC6be4325173F976aC4A4e5b91154B90B601CF1'; // Replace with the actual token address
+const contractAddress = '0xa3aba6FC76E33f80138b6d4AB2592968FA1A4Df6'; // Replace with the actual staking contract address
+
+const Staking = ({ account }) => {
+  const { library } = useWeb3React();
+
   const [stakeAmount, setStakeAmount] = useState("");
   const [unstakeAmount, setUnstakeAmount] = useState("");
   const [balance, setBalance] = useState(0);
+  const [contract, setContract] = useState();
+  const [tokenContract, setTokenContract] = useState();
 
   useEffect(() => {
-    if (contract && account) {
-      // Fetch user's staked balance from the contract
-      contract.balanceOf(account).then((result) => {
-        setBalance(result.toString());
-      });
-    }
-  }, [contract, account]);
+    if (library) setup(library);
+  }, [library, account]);
+
+  const setup = async (_library) => {
+    const provider = _library.getSigner();
+    const stakingContract = new ethers.Contract(contractAddress, stakingABI, provider);
+    setContract(stakingContract);
+
+    const _tokenContract = new ethers.Contract(tokenAddress, tokenABI, provider);
+    setTokenContract(_tokenContract);
+
+    stakingContract.stakedBalance(account).then((result) => {
+      setBalance(result.toString());
+    });
+
+    // Listen to the "Staked" event
+    stakingContract.on("Staked", (user, amount) => {
+      console.log("Staked event:", user, amount);
+      // Update your UI or state accordingly
+    });
+
+    // Listen to the "Withdrawn" event
+    stakingContract.on("Withdrawn", (user, amount) => {
+      console.log("Withdrawn event:", user, amount);
+      // Update your UI or state accordingly
+    });
+  };
 
   const handleStake = async () => {
-    if (stakeAmount > 0) {
-      await contract.stake(stakeAmount);
-      setStakeAmount("");
+    if (stakeAmount > 0 && contract && tokenContract) {
+      try {
+        const stakeAmountBN = ethers.utils.parseUnits(stakeAmount, 18);
+
+        // Approve staking contract to spend tokens
+        const approveTx = await tokenContract.approve(contract.address, stakeAmountBN);
+        await approveTx.wait();
+
+        // Stake the tokens
+        const stakeTx = await contract.stake(stakeAmountBN);
+        await stakeTx.wait();
+
+        setStakeAmount("");
+      } catch (error) {
+        console.error("Error staking:", error);
+      }
     }
   };
 
   const handleUnstake = async () => {
-    if (unstakeAmount > 0) {
-      await contract.unstake(unstakeAmount);
-      setUnstakeAmount("");
+    if (unstakeAmount > 0 && contract) {
+      try {
+        const unstakeAmountBN = ethers.utils.parseUnits(unstakeAmount, 18);
+
+        // Unstake the tokens
+        const unstakeTx = await contract.withdraw(unstakeAmountBN);
+        await unstakeTx.wait();
+
+        setUnstakeAmount("");
+      } catch (error) {
+        console.error("Error unstaking:", error);
+      }
     }
+  };
+
+  const formatBalance = (balance) => {
+    const tokenDecimals = 18;
+    const balanceInToken = balance / 10 ** tokenDecimals;
+    const formattedBalance = balanceInToken.toLocaleString(undefined, {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    });
+    return formattedBalance;
   };
 
   return (
     <div className="staking-container">
       <h2>Staking</h2>
       <p>Your Account: {account}</p>
-      <p>Your Staked Balance: {balance}</p>
-      
+      <p>Your Staked Balance: {formatBalance(balance)}</p>
+
       <div className="action-section">
         <div>
           <label>Stake Amount:</label>
@@ -43,10 +106,9 @@ const Staking = ({ contract, account }) => {
             value={stakeAmount}
             onChange={(e) => setStakeAmount(e.target.value)}
           />
-          <button onClick={handleStake} >Stake</button>
+          <button onClick={handleStake}>Stake</button>
         </div>
-  
-        
+
         <div>
           <label>Unstake Amount:</label>
           <input
@@ -54,7 +116,9 @@ const Staking = ({ contract, account }) => {
             value={unstakeAmount}
             onChange={(e) => setUnstakeAmount(e.target.value)}
           />
-          <button onClick={handleUnstake}  style={{ marginLeft: "10px" }}>Unstake</button>
+          <button onClick={handleUnstake} style={{ marginLeft: "10px" }}>
+            Unstake
+          </button>
         </div>
       </div>
     </div>
